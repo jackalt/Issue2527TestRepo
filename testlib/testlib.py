f7056b1d36031f91cd5279e0077639f1542265fe
jack1142 <6032823+jack1142@users.noreply.github.com>
2019-04-10 16:23:15 +0200

    only lib updated

--- testlib/testlib.py
+++ testlib/testlib.py
@@ -1,3 +1,8 @@
 def add_numbers(x, y):
     """Kinda useless function, but works fine for testing :P"""
     return x + y
+
+
+def multiply_numbers(x, y):
+    """Kinda useless function, but works fine for testing :P"""
+    return x * y
