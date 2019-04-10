73377174ef3cda2c41ca85897187a61ac93c1aca
jack1142 <6032823+jack1142@users.noreply.github.com>
2019-04-10 16:23:55 +0200

    only cog updated

--- testcog/testcog.py
+++ testcog/testcog.py
@@ -17,3 +17,10 @@ class TestCog(commands.Cog):
         """Settings for voice tools"""
         result = testlib.add_numbers(x, y)
         await ctx.send(f"Result of {x} + {y} is {result}")
+
+    @commands.is_owner()
+    @commands.command()
+    async def anothercommand(self, ctx, x: int, y: int):
+        """Settings for voice tools"""
+        result = testlib.multiply_numbers(x, y)
+        await ctx.send(f"Result of {x} * {y} is {result}")
