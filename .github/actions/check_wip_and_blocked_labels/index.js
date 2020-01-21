6431b9cb1d9fd402d75239d42d654a69c831168b
jack1142 <6032823+jack1142@users.noreply.github.com>
2020-01-22 00:22:05 +0100

    Update index.js

--- .github/actions/check_wip_and_blocked_labels/index.js
+++ .github/actions/check_wip_and_blocked_labels/index.js
@@ -2,15 +2,18 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 
 async function run() {
+    // almost like tuple unpacking lol
+    const [repo_owner, repo_name] = process.env.GITHUB_REPOSITORY.split("/");
+    const github_sha = process.env.GITHUB_SHA;
     const github_token = core.getInput("github-token");
 
     const octokit = new github.GitHub(github_token);
     
     octokit.checks.create({
         name: 'No WIP and Blocked labels',
-        owner: context.repo.owner,
-        repo: context.repo.repo,
-        head_sha: context.pull_request.head.sha,
+        owner: repo_owner,
+        repo: repo_name,
+        head_sha: github_sha,
         status: 'completed',
         conclusion: conclusion
     });
