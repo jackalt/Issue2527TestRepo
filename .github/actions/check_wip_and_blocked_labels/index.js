540a9fac8ef1882c50ac5afbcd3865d21df1647a
jack1142 <6032823+jack1142@users.noreply.github.com>
2020-01-22 01:19:01 +0100

    Update index.js

--- .github/actions/check_wip_and_blocked_labels/index.js
+++ .github/actions/check_wip_and_blocked_labels/index.js
@@ -2,29 +2,29 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 
 async function run() {
-    const context = github.context
-    const github_token = core.getInput("github-token");
+    try {
+        const context = github.context
+        const github_token = core.getInput("github-token");
 
-    const octokit = new github.GitHub(github_token);
-    
-    const is_blocked_label = (label) => label.name.startsWith('Blocked') || label.name == 'Work In Progress';
-    let conclusion = 'success';
-    if (context.payload.pull_request.labels.some(is_blocked_label)) {
-        conclusion = 'failure';
+        const octokit = new github.GitHub(github_token);
+
+        const is_blocked_label = (label) => label.name.startsWith('Blocked') || label.name == 'Work In Progress';
+        let conclusion = 'success';
+        if (context.payload.pull_request.labels.some(is_blocked_label)) {
+            conclusion = 'failure';
+        }
+
+        octokit.checks.create({
+            name: 'No WIP and Blocked labels',
+            owner: context.repo.owner,
+            repo: context.repo.repo,
+            head_sha: context.payload.pull_request.head.sha,
+            status: 'completed',
+            conclusion: conclusion
+        });
+    } catch (error) {
+        core.setFailed(error.message);
     }
-    
-    octokit.checks.create({
-        name: 'No WIP and Blocked labels',
-        owner: context.repo.owner,
-        repo: context.repo.repo,
-        head_sha: context.payload.pull_request.head.sha,
-        status: 'completed',
-        conclusion: conclusion
-    });
 }
 
-try {
-    run();
-} catch (error) {
-    core.setFailed(error.message);
-}
+run();
